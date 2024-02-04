29b629d0c116b3c92bfc04ddca6755cac3b247bd
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-04 15:25:39 -0400

    Creando el Servico Menu

---
+++ PizzaPlace/PizzaPlace/Services/MenuService.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlace.Models;
+
+namespace PizzaPlace.Services
+{
+    public class MenuService
+    {
+        public List<Pizzas> Pizzas { get; set; } = new List<Pizzas>();
+        public void Add(Pizzas pizza) => Pizzas.Add(pizza);
+        public Pizzas? GetPizza(int id) => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+
+}
