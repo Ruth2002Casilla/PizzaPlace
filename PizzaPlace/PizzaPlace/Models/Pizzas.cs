5cccfacac529ef6b33d174d790520ceab61c5223
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-04 15:24:17 -0400

    Creando el modelo Pizzas

---
+++ PizzaPlace/PizzaPlace/Models/Pizzas.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaPlace.Models
+{
+    public class Pizzas
+    {
+        public Pizzas(int Id, string Nombre, decimal Precio, Picantes Picante)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.Precio = Precio;
+            this.Picante = Picante;
+        }
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public Picantes Picante { get; }
+    }
+}
