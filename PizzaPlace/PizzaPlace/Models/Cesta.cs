726090111b167547a5be31010413e929e4dc153f
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-04 15:16:19 -0400

    Creando el modelo de Cesta.

---
+++ PizzaPlace/PizzaPlace/Models/Cesta.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaPlace.Models
+{
+    public class Cestas
+    {
+        public Clientes Cliente { get; set; } = new Clientes();
+        public List<int> Pedidos { get; set; } = new List<int>();
+        public bool Pago { get; set; }
+        public void Add(int pizzaId) => Pedidos.Add(pizzaId);
+        public void RemoveAt(int pos) => Pedidos.RemoveAt(pos);
+    }
+}
