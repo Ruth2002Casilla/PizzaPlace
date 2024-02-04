994815624a8db7ab62654a18280e649d57d57915
Ruth2002Casilla <ruth.b2002casillagarcia@gmail.com>
2024-02-04 15:20:49 -0400

    Creando el Modelo Estados

---
+++ PizzaPlace/PizzaPlace/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlace.Services;
+
+namespace PizzaPlace.Models
+{
+    public class Estados
+    {
+        public MenuService Menu { get; } = new MenuService();
+        public Cestas Cesta { get; } = new Cestas();
+        public Interfaz Interfaz { get; set; } = new Interfaz();
+        public decimal PrecioTotal => Cesta.Pedidos.Sum(Id => Menu.GetPizza(Id)!.Precio);
+    }
+}
